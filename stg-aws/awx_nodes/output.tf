38f5b05049f0c0d29d1e0ab9cddbc72d5a865ee0
xiwonpark <siwon1002@naver.com>
2022-11-17 18:34:41 +0900

    update: awx_nodes_output

--- stg-aws/awx_nodes/output.tf
+++ stg-aws/awx_nodes/output.tf
@@ -1,7 +1,15 @@
-output "awx_node01" {
-    value = aws_instance.awx_node01.id
+output "ldapserver" {
+    value = aws_instance.ldapserver.id
 }
 
-output "awx_node02" {
-    value = aws_instance.awx_node02.id
+output "vncserver" {
+    value = aws_instance.vncserver.id
+}
+
+output "lsfserver" {
+    value = aws_instance.lsfserver.id
+}
+
+output "lsfnode01" {
+    value = aws_instance.lsfnode01.id
 }
